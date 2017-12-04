9e4e7b714fd3bad8e454b8f88210817381df427d
jbeetz <jbeetz@hotmail.com>
2017-12-03 22:58:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/i', (req, res) => {
-    res.send({"hello":"world"});
+    res.send({hello:"world"});
 });
 
-app.listen(9600, function(){
+app.listen(process.env.PORT, function(){
     console.log('server has started on port 9600')
 });
